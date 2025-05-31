1159460ba287e303ac412691405300b0b1b6bd47
Bruna Maria Duarte Barbosa <brmaria-@c2r1s9.42porto.com>
2025-05-31 18:58:39 +0100

    practicing a little bit more

---
+++ level-3/epur_str.c
@@ -0,0 +1,16 @@
+#include <unistd.h>
+int	main(int argc, char *argv)
+{
+	if (argc == 2)
+	{	
+		int	i = 0;
+		int	words = 0;
+		int	end = count_words(argv[1]);
+		while (argv[1][i])
+		{
+			if (argv[1][i])
+		}
+	}
+	write (1, "\n", 1);
+	return (0);
+}
