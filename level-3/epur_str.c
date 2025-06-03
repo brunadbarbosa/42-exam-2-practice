643a87e15918bf9e28bf2461f634490e10db4e0b
Bruna Barbosa <duar0029@algonquinlive.com>
2025-06-03 10:46:41 +0100

    Finishing level3

--- level-3/epur_str.c
+++ level-3/epur_str.c
@@ -1,5 +1,23 @@
 #include <unistd.h>
-int	main(int argc, char *argv)
+int	count_words(char *str)
+{
+	int	i = 0;
+	int	count = 0;
+	while (str[i])
+	{
+		if (str[i] != ' ' && str[i] != '\t')
+		{
+			count++;
+			while (str[i] && (str[i] != ' ' && str[i] != '\t'))
+				i++;
+		}
+		else
+			i++;
+	}
+	return (count);
+
+}
+int	main(int argc, char **argv)
 {
 	if (argc == 2)
 	{	
@@ -8,7 +26,19 @@ int	main(int argc, char *argv)
 		int	end = count_words(argv[1]);
 		while (argv[1][i])
 		{
-			if (argv[1][i])
+			if (argv[1][i] != ' ' && argv[1][i] != '\t')
+			{
+				words++;
+				while (argv[1][i] && (argv[1][i] != ' ' && argv[1][i] != '\t'))
+				{
+					write (1, &argv[1][i], 1);
+					i++;
+				}
+				if (words < end)
+					write (1, " ", 1);
+			}
+			else
+				i++;
 		}
 	}
 	write (1, "\n", 1);
---
+++ level-3/hidenp.c
@@ -0,0 +1,13 @@
+int	main (int argc, char **argv)
+{
+	int	i = 0;
+	if (argc == 2)
+	{
+		while (argv[2][i])
+		{
+			
+		}
+	}
+	write (1, "\n", 1);
+	return (0);
+}
